c37369a7229cbee7f3e080a273ca30440d3b7fec
Adrian Pieper <gergoybey@gmail.com>
2016-05-13 16:21:23 +0200

    angular official tutorial finished

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,49 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+
+
+])
 @Component({
   selector: 'my-app',
-  template: '<h1>My SECOND Angular 2 app</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css'],
+
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
+
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
